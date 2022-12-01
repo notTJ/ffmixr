85ce81fa0976a6ee5a44e6cde6bfd4abbca4b455
TJ <tj.hersheyy@gmail.com>
2022-11-30 22:19:28 -0500

    include rescore file

--- src/rescore.ts
+++ src/rescore.ts
@@ -1,9 +1,12 @@
 import { createFFmpegWithDefaultListeners } from "./default-configuration.ts";
 import { eventStream } from "./event-stream.ts";
-import { ChannelSplitFilter } from "./filters/channel-split/channel-split-filter.ts";
-import { Resolution } from "../../deno-fast-forward/src/mod.ts";
-import {ChannelLayoutMap} from "./filters/channel-split/channel-layout-map.ts";
-import {DefaultChannelOutputName} from "./filters/channel-split/channel.ts";
+import { ChannelsplitFilter } from "./filters/channel-split/channelsplit-filter.ts";
+import { MappedOutput, Resolution } from "../../deno-fast-forward/src/mod.ts";
+import {
+  Channel,
+  DefaultChannelOutputName,
+} from "./filters/channel-split/channel.ts";
+import { ChannelLayoutMap } from "./filters/channel-split/channel-layout-map.ts";
 /*
 Steps
 1. trim video and track
@@ -13,19 +16,19 @@ Steps
 the two cutting steps may be able to be run in the same promise
 */
 // music track
-// const trackCut = createFFmpegWithDefaultListeners(false)
-//   .input('./fixtures/I LikeIt.flac')
-//   // Global encoding options (applied to all outputs).
-//   // .audioBitrate("192k")
-//   // .videoBitrate("1M")
-//   // .noInputVideo()
-//   // .inputArgs()
-//   // Output 1.
-//   .output('./fixtures/ILikeIt-cut.mp3')
-//   .start('00:00:04.5')
-//   .end('00:01:22.0')
-//   .encode();
-// await eventStream(trackCut);
+const trackCut = createFFmpegWithDefaultListeners(false)
+  .input("./fixtures/I Like It.flac")
+  // Global encoding options (applied to all outputs).
+  // .audioBitrate("192k")
+  // .videoBitrate("1M")
+  // .noInputVideo()
+  // .inputArgs()
+  // Output 1.
+  .output("./fixtures/I Like It-cut.mp3")
+  .seek("00:00:04.5")
+  .to("00:01:22.0");
+// .encode();
+await eventStream(trackCut);
 
 // video
 // streams: 0: film, 1: audio 7.1 truehd, 2: ac3 5.1(side),  3: steroeo, 4+ subtitles
@@ -42,31 +45,27 @@ const videoCut = await createFFmpegWithDefaultListeners()
   .noSubtitles()
   .format("ac3")
   .complexFilter(
-    new ChannelSplitFilter()
+    new ChannelsplitFilter()
       .setLayout("7.1")
-      .setChannels(ChannelLayoutMap["7.1"])
-      .build()
-  ).mappedOutputs(ChannelLayoutMap["7.1"].map((channel) => {
+      .referenceAllChannels()
+      // .setChannels(Channel.frontCenter)
+      .toString(),
+  ) //.mappedOutputs({ filename: "zfc.mkv", identifier: "[FC]"})
+  // separate audio track into each channel
+  .mappedOutputs(...ChannelLayoutMap["7.1"].map((channel) => {
     return {
       identifier: `[${channel}]`,
       filename: `${DefaultChannelOutputName[channel]}.ac3`,
-    }
-  }))
+    } as MappedOutput;
+  }));
 // .inputAudioChannels(3)
 // Start encoding.
 // await videoCut.encode();
 await eventStream(videoCut);
 
+// put the video + audio together
 
-// put into deno-ff tests
-// .filter(
-//   new ChannelSplitFilter()
-//     .splitAllChannelsIntoOutputs("7.1")
-//     .setMappedOutputFileExt(".ac3")
-//     .buildFilter(),
-// );
 // ffmpeg -hide_banner -ss 00:00:05.0 -to 00:00:15.0 -i ./Zombieland.mkv -loglevel error -y -vn -sn -filter_complex channelsplit=channel_layout=7.1[FL][FR][FC][LFE][BL][BR][SL][SR] -map [FL] front_left.ac3 -map [FR] front_right.ac3 -map [FC] front_center.ac3 -map [LFE] low_frequency.ac3 -map [BL] back_left.ac3 -map [BR] back_right.ac3 -map [SL] side_left.ac3 -map [SR] side_right.ac3
-
 
 /*
 finding log/param info inside ffmpeg
